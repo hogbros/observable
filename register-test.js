0f264b608c5cfe1f422e536edaa8f2241b1bc495
Joshua Hogsett <jthogsett@gmail.com>
2019-04-01 21:27:09 -0400

    add more unit tests

--- register-test.js
+++ register-test.js
@@ -1,3 +1,6 @@
 require("ts-node").register({
   project: "test/tsconfig.json"
 });
+const chai = require("chai");
+const spies = require("chai-spies");
+chai.use(spies);
--- test/observable.spec.ts
+++ test/observable.spec.ts
@@ -1,9 +1,30 @@
 import { Observable } from "../src/observable";
-import { assert } from "chai";
+import { expect, spy } from "chai";
 
 describe("Observable", function() {
-    it("should only create one Observable implementation per class hierarchy", function() {
-        const myObservable = Observable(Object);
-        assert.strictEqual(Observable(myObservable), myObservable);
-    });
+  it("should only create one Observable implementation per class hierarchy", function() {
+    const myObservable = Observable(Object);
+    expect(Observable(myObservable)).to.deep.equal(myObservable);
+  });
+  it("calls the propertyChangedCallback when an observed property is set", function() {
+    const MyObservable = Observable(Object);
+    MyObservable.observeProperty("myObservedProperty");
+    const myObservable = new MyObservable();
+    const propertyChangedCallbackSpy = spy.on(
+      myObservable,
+      "propertyChangedCallback"
+    );
+    (myObservable as any).myObservedProperty = "Value 1";
+    expect(propertyChangedCallbackSpy).to.have.been.called.with(
+      "myObservedProperty",
+      undefined,
+      "Value 1"
+    );
+    (myObservable as any).myObservedProperty = "Value 2";
+    expect(propertyChangedCallbackSpy).to.have.been.called.with(
+      "myObservedProperty",
+      "Value 1",
+      "Value 2"
+    );
+  });
 });
